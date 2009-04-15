177db509d48e5ff409bf06692b685e7af1b7f663
Steve Plimpton <sjplimp@sandia.gov>
2009-04-15 14:35:19 +0000

    git-svn-id: svn+ssh://software-srn.sandia.gov/svn/mapreduce/trunk@204 3fc257bb-8550-0410-81b3-a5c103da83f7

--- doc/PDFgen.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/csh
-# generate a PDF version of Manual
-
-txt2html -b *.txt >! mapreduce.html
-
-/home/sjplimp/tools/htmldoc/bin/htmldoc --title --toctitle "Table of Contents" --tocfooter ..i --toclevels 4 --header ... --footer ..1 --size letter --linkstyle plain --linkcolor blue -f mapreduce.pdf mapreduce.html
-
-txt2html *.txt >! mapreduce.html
