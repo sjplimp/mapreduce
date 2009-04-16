dd10347f1e39e01bb2e0b7096f3df2da3d58ad51
Steve Plimpton <sjplimp@sandia.gov>
2009-04-16 21:05:10 +0000

    git-svn-id: svn+ssh://software-srn.sandia.gov/svn/mapreduce/trunk@211 3fc257bb-8550-0410-81b3-a5c103da83f7

--- mpistubs/mpi.h
+++ mpistubs/mpi.h
@@ -108,9 +108,9 @@ void MPI_Gather(void *sendbuf, int sendcount, MPI_Datatype sendtype,
 		void *recvbuf, int recvcount, MPI_Datatype recvtype,
 		int root, MPI_Comm comm);
 
-#endif
-
 #ifdef __cplusplus
 }
+#endif
+
 #endif
 
