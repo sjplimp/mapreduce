a2349df20b705b19490e0ee75d7030ca6c8a6eb6
Steve Plimpton <sjplimp@sandia.gov>
2011-02-07 23:46:37 +0000

    git-svn-id: svn+ssh://software-srn.sandia.gov/svn/mapreduce/trunk@790 3fc257bb-8550-0410-81b3-a5c103da83f7

--- oinkdoc/PDFgen.sh
+++ oinkdoc/PDFgen.sh
@@ -4,6 +4,6 @@
 python Manual.py
 txt2html -b *.txt
 
-/home/sjplimp/tools/htmldoc/bin/htmldoc --title --toctitle "Table of Contents" --tocfooter ..i --toclevels 4 --header ... --footer ..1 --size letter --linkstyle plain --linkcolor blue -f Manual.pdf Manual.html Section_build.txt Section_script.txt Section_functions.txt Section_commands.txt Section_errors.txt [a-z]*.txt
+/home/sjplimp/tools/htmldoc/bin/htmldoc --title --toctitle "Table of Contents" --tocfooter ..i --toclevels 4 --header ... --footer ..1 --size letter --linkstyle plain --linkcolor blue -f Manual.pdf Manual.html Section_build.html Section_script.html Section_functions.html Section_commands.html Section_errors.html [a-z]*.html
 
 txt2html *.txt
