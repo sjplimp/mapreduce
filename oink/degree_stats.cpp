abc558106305e1b67268b1890251553303cee7c1
Steve Plimpton <sjplimp@sandia.gov>
2011-03-08 16:13:34 +0000

    git-svn-id: svn+ssh://software-srn.sandia.gov/svn/mapreduce/trunk@824 3fc257bb-8550-0410-81b3-a5c103da83f7

---
+++ oink/degree_stats.cpp
@@ -0,0 +1,81 @@
+/* ----------------------------------------------------------------------
+   OINK - scripting wrapper on MapReduce-MPI library
+   http://www.sandia.gov/~sjplimp/mapreduce.html, Sandia National Laboratories
+   Steve Plimpton, sjplimp@sandia.gov
+
+   See the README file in the top-level MR-MPI directory.
+------------------------------------------------------------------------- */
+
+#include "mpi.h"
+#include "stdio.h"
+#include "stdlib.h"
+#include "degree_stats.h"
+#include "typedefs.h"
+#include "object.h"
+#include "style_map.h"
+#include "style_reduce.h"
+#include "error.h"
+
+#include "mapreduce.h"
+#include "keyvalue.h"
+
+using namespace OINK_NS;
+using namespace MAPREDUCE_NS;
+
+#define MAXLINE 1024
+
+/* ---------------------------------------------------------------------- */
+
+DegreeStats::DegreeStats(OINK *oink) : Command(oink)
+{
+  ninputs = 1;
+  noutputs = 0;
+}
+
+/* ---------------------------------------------------------------------- */
+
+void DegreeStats::run()
+{
+  int me,nprocs;
+  MPI_Comm_rank(MPI_COMM_WORLD,&me);
+  MPI_Comm_size(MPI_COMM_WORLD,&nprocs);
+
+  // mre = graph edges
+
+  MapReduce *mre = obj->input(1,read_edge,NULL);
+  MapReduce *mr = obj->create_mr();
+  uint64_t nedge = mre->kv_stats(0);
+
+  mr->map(mre,edge_to_vertices,NULL);
+  mr->collate(NULL);
+  uint64_t nvert = mr->reduce(count,NULL);
+  mr->map(mr,invert,NULL);
+  mr->collate(NULL);
+  mr->reduce(count,NULL);
+  mr->gather(1);
+  mr->sort_keys(-1);
+
+  char msg[128];
+  sprintf(msg,"DegreeStats: %d vertices, %d edges",nvert,nedge);
+  if (me == 0) error->message(msg);
+  mr->scan(print,NULL);
+
+  obj->cleanup();
+}
+
+/* ---------------------------------------------------------------------- */
+
+void DegreeStats::params(int narg, char **arg)
+{
+  if (narg != 0) error->all("Illegal degree_stats command");
+}
+
+/* ---------------------------------------------------------------------- */
+
+void DegreeStats::print(char *key, int keybytes, char *value,
+			int valuebytes, void *ptr)
+{
+  int nedge = *(int *) key;
+  int nvert = *(int *) value;
+  printf("  %d vertices with %d edges\n",nvert,nedge);
+}
