1a03186a090221e51610eef9442e74052e1f59c5
Karen Devine <kddevin@sandia.gov>
2011-03-09 22:23:07 +0000

    First attempt at oinked sssp. Not tested yet, but it compiles. :)
    git-svn-id: svn+ssh://software-srn.sandia.gov/svn/mapreduce/trunk@860 3fc257bb-8550-0410-81b3-a5c103da83f7

--- oink/sssp.h
+++ oink/sssp.h
@@ -17,35 +17,84 @@ CommandStyle(sssp,SSSP)
 
 #include "command.h"
 #include "keyvalue.h"
+#include "float.h"
+#include <vector>
+
 using MAPREDUCE_NS::KeyValue;
 
 namespace OINK_NS {
 
+typedef void REDUCE1_FN(char*, int, char*, int, int*, KeyValue*, void*);
+typedef void REDUCE2_FN(char*, int, char*, int, void*);
+typedef void MAP1_FN(int, KeyValue*, void*);
+typedef void MAP2_FN(uint64_t, char*, int, char*, int, KeyValue*, void*);
+
+//////////////////////////////////////////////////////////////////////////
+class EDGEVALUE{
+// Edge with destination vertex (8-bytes) and edge weight
+// Given a key that is a VERTEX Vi, EDGEVALUE contains Vj and the weight
+// of edge Vi->Vj.
+public:
+  VERTEX v;
+  WEIGHT wt;
+  friend std::ostream& operator<<(std::ostream& output, const EDGEVALUE& e) {
+    output << e.v << " " << e.wt;
+    return output;
+  };
+  friend bool operator!=(const EDGEVALUE& lhs, const EDGEVALUE& rhs) {
+    if ((lhs.wt != rhs.wt) || (lhs.v != rhs.v)) return true;
+    return false;
+  };
+};
+
+class DISTANCE {
+// Class used to pass distance information through the MapReduce system.
+public:
+  DISTANCE(){
+    memset(&(e.v), 0, sizeof(VERTEX));
+    e.wt = FLT_MAX;
+    current = true;
+  };
+  ~DISTANCE(){};
+  EDGEVALUE e;   // Edge describing the distance of a vtx from S; 
+                 // e.v is predecessor vtx; e.wt is distance from S through e.v.
+  bool current;  // Flag indicating that this distance is the current state
+                 // for the vtx (the currently accepted best distance).
+                 // Needed so we can know when to stop (when no vtx distances
+                 // change in an iteration).
+  friend bool operator!=(const DISTANCE& lhs,
+                         const DISTANCE& rhs) {
+    if (lhs.e != rhs.e) return true;
+    return false;
+  };
+};
+
+//////////////////////////////////////////////////////////////////////////
 class SSSP : public Command {
  public:
   SSSP(class OINK *);
   void run();
   void params(int, char **);
+  int me;
+  int np;
 
  private:
-  int niterate,seed;
+  int ncnt;  // Number of SSSP computations to do.
+  int seed;  // Random seed initialization
+  static uint64_t NVtxLabeled;   // Number of local vertices labeled so far.
 
-  static void print(char *, int, char *, int, void *);
+  std::vector<VERTEX> sourcelist; // ncnt sources with outdegree > zero.
 
-  /*
-  static void map_edge_vert(uint64_t, char *, int, char *, int,
-			    KeyValue *, void *);
-  static void reduce_first_degree(char *, int, char *, int, int *, 
-				  KeyValue *, void *);
-  static void reduce_second_degree(char *, int, char *, int, int *, 
-				   KeyValue *, void *);
-  static void map_low_degree(uint64_t, char *, int, char *, int,
-			     KeyValue *, void *);
-  static void reduce_nsq_angles(char *, int, char *, int, int *, 
-				KeyValue *, void *);
-  static void reduce_emit_triangles(char *, int, char *, int, int *, 
-				    KeyValue *, void *);
-  */
+  bool get_next_source(VERTEX *, int);
+
+  static MAP1_FN add_source;
+  static MAP2_FN reorganize_edges;
+  static MAP2_FN move_to_new_mr;
+  static MAP2_FN initialize_vertex_distances;
+  static REDUCE1_FN get_good_sources;
+  static REDUCE1_FN pick_shortest_distances;
+  static REDUCE1_FN update_adjacent_distances;
+  static REDUCE2_FN print;
 };
 
 }
