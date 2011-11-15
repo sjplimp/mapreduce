552d947bd3d4c28c91bb1962dfe2a43f86bb9f70
Steve Plimpton <sjplimp@sandia.gov>
2011-11-15 00:47:45 +0000

    git-svn-id: svn+ssh://software-srn.sandia.gov/svn/mapreduce/trunk@961 3fc257bb-8550-0410-81b3-a5c103da83f7

---
+++ phish/minnow/readgraph.py
@@ -0,0 +1,24 @@
+#!/usr/local/bin/python
+
+import sys,os,glob,copy
+from phish import Phish
+
+phish = Phish()
+
+args = phish.init(sys.argv)
+phish.output(0)
+phish.check()
+
+if len(args) != 1: phish.error("Readgraph syntax: readgraph.py file")
+
+infile = args[0]
+lines = open(infile,"r").readlines()
+
+for line in lines:
+  words = line.split()
+  phish.pack_byte(words[0][0])
+  ivec = [int(word) for word in words[1:]]
+  phish.pack_int_array(ivec)
+  phish.send(0)
+  
+phish.exit()
