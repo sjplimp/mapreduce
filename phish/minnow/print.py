afd228eb592f2051ae0c226c7ed25a93febfd88c
Steve Plimpton <sjplimp@sandia.gov>
2011-11-15 21:52:33 +0000

    git-svn-id: svn+ssh://software-srn.sandia.gov/svn/mapreduce/trunk@971 3fc257bb-8550-0410-81b3-a5c103da83f7

--- phish/minnow/print.py
+++ phish/minnow/print.py
@@ -9,14 +9,24 @@ def myprint(nvalues):
     if type == phish.RAW:
       pass
     elif type < phish.INT_ARRAY:
-      print value,
+      print >>fp,value,
     else:
-      for val in value: print val,
-  print
-  
+      for val in value: print >>fp,val,
+  print >>fp
+
 args = phish.init(sys.argv)
 phish.input(0,myprint,None,1)
 phish.check()
+
+fp = sys.stdout
+
+iarg = 0
+while iarg < len(args):
+  if args[iarg] == "-f":
+    if iarg+1 > len(args): phish.error("Print syntax: print -f filename");
+    fp = open(args[iarg+1],"w")
+    iarg += 2
+  else: phish.error("Print syntax: print -f filename")
 
 phish.loop()
 phish.exit()
