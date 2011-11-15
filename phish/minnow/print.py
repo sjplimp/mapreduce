f2bac301826ac08c15296b643baa7686890a65b5
Steve Plimpton <sjplimp@sandia.gov>
2011-11-15 21:32:35 +0000

    git-svn-id: svn+ssh://software-srn.sandia.gov/svn/mapreduce/trunk@969 3fc257bb-8550-0410-81b3-a5c103da83f7

--- phish/minnow/print.py
+++ phish/minnow/print.py
@@ -6,9 +6,14 @@ import phish
 def myprint(nvalues):
   for i in range(nvalues):
     type,value,len = phish.unpack()
-    print value,
+    if type == phish.RAW:
+      pass
+    elif type < phish.INT_ARRAY:
+      print value,
+    else:
+      for val in value: print val,
   print
-
+  
 args = phish.init(sys.argv)
 phish.input(0,myprint,None,1)
 phish.check()
